7e2d54a2a7b917462b41bb845a758b8635343d40
Bruno Martins <bgcngm@gmail.com>
2018-04-27 00:04:21 +0100

    davinci: List additional native libraries to expose to apps
     * Initially based on the list defined by Xiaomi, trimmed to include
       only the vendor libs that are also being shipped by us.
    
    Change-Id: I552dc44a32221d2d3cec48e1c64ed72c470d412e

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/davinci/davinci-vendor.mk)
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
