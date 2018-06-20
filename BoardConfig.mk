f74425846cfffc2e3b47cddf20b4f3ea96adb955
Dan Willemsen <dwillemsen@google.com>
2018-06-19 22:28:20 -0700

    davinci: Builds have overriding commands
    This product has warnings about overriding build commands, which
    means that there are multiple instructions on how to build a file, and
    we've just picked one of them. Which one we pick is usually stable, but
    it can change.
    
    Bug: 77611511
    Test: turn default to false, davinci still uses a warning
    Change-Id: I11d194863b313f64b19291f65219682b169d9ced

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,5 +18,7 @@ BOARD_VENDOR := xiaomi
 
 DEVICE_PATH := device/xiaomi/davinci
 
+BUILD_BROKEN_DUP_RULES := true
+
 # Inherit from proprietary files
 include vendor/xiaomi/davinci/BoardConfigVendor.mk
