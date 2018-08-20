8f5b2b6b3ad16f1ab6cd4ac49d8d510ff3686f4a
Adrian Roos <roosa@google.com>
2018-08-20 16:13:04 +0200

    violet: Add overlay for masking the notch
    Bug: 112876936
    Test: Go to Settings > Developer Options > Simulate display with cutout > "No cutout". Cutout should be hidden. Rotate screen, take screenshots, screenrecord should all work.
    Change-Id: I66684d8254fd3c1ee6851e67c0b09d176d4c02af
    (cherry picked from commit dcc48dc9071c37c8b9fe7caee96585d20d149060)
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: K A R T  H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -331,6 +331,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
     $(LOCAL_PATH)/configs/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service
