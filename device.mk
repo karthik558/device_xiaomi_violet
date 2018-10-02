65d8db50aae9bdb25b3b9554d9cacb99a2a4ad41
Bruno Martins <bgcngm@gmail.com>
2018-10-02 21:57:25 +0100

    davinci: textclassifier: Build smart selection models
    Change-Id: I4de9326fe60a395454834906552518afbc376f49

--- device.mk
+++ device.mk
@@ -285,6 +285,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
