e551c833becffaa1059ba6f0c4189994cecbd0ff
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    davinci: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>

--- device.mk
+++ device.mk
@@ -359,6 +359,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Uevent
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
