89fa122552bbc06936c79a5991550004fd2842bd
Indranil Chakraborty <ichakrab@codeaurora.org>
2019-02-21 23:57:02 +0530

    violet: Pack libwfdaac_vendor for WFD
    Change-Id: I828918c42e1eb2963829d62a9dbeee83e2f0e600

--- device.mk
+++ device.mk
@@ -460,7 +460,7 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     libqdMetaData.vendor \
-    libwfdaac_proprietary
+    libwfdaac_vendor
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
