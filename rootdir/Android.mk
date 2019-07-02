6cc06215c007f1a27c5ef9cd1930391d61c351af
Rizky Benggolo <travarilo@gmail.com>
2019-07-02 13:02:32 +0000

    violet: Enable & Fix Smart Charging
    - add device specific init script
    - add sysfs nodes
    - set permissions
    - address denial
    
    Signed-off-by: Logan <athuldineshan5@gmail.com>
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,4 @@
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -92,4 +93,12 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/init.violetparts.sh
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
