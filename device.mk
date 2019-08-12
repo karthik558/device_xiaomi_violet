5ca0a9fff70ce2e54682619f6d243d7514178f29
Demon000 <demonsingur@gmail.com>
2019-08-12 06:11:58 +0200

    davinci: display: Build stack from source
    Change-Id: I4ae143d0cc0fba379894e1f69b793312192f0c4a

--- device.mk
+++ device.mk
@@ -108,6 +108,19 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-service \
+    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    vendor.qti.hardware.display.allocator-service
+
+PRODUCT_PACKAGES += \
+    gralloc.sm6150 \
+    hwcomposer.sm6150 \
+    memtrack.sm6150
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.davinci
