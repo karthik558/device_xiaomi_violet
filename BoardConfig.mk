cce805e116541fbed6c1cbcd1d3d39f639c1070a
AbhiShek Aggarwal <warabhishek@gmail.com>
2019-08-26 17:40:14 -0400

    davinci: Add custom init to set proper variants
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: I04f09a14cf41c89b3135c53e3089d5c3e703aae0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,10 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_davinci
+TARGET_RECOVERY_DEVICE_MODULES := libinit_davinci
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
