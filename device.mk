6cb6f214bcb720a8a1c8e3220f95708eece34c68
Tharaga Balachandran <tbalacha@codeaurora.org>
2020-01-08 17:17:56 -0500

    davinci: gralloc: Add support for IMapper/IAllocator 4.0
    * Add QtiMapper and QtiAllocator 4.0
    * Implement IMapper 4.0 APIs: get, set,
      getFromBufferDescriptorInfo, flushLockedBuffer,
      rereadLockedBuffer, listSupportedMetadataTypes,
      dumpBuffer, dumpBuffers, and getReservedRegion
    
    CRs-Fixed: 2633272
    Change-Id: Ica42e485e426b366ffb41f30f1ea2cf395901356
    Signed-off-by: Baldev Sahu <bsahu@codeaurora.org>

--- device.mk
+++ device.mk
@@ -133,6 +133,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.allocator-service
