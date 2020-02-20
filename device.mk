dec8edf4c5bf810d8b5c35697eebaea7b5146048
Vishalcj17 <vishalcj@aospa.co>
2020-02-20 14:56:59 +0530

    violet: Drop perf daemon and thermal
    Change-Id: I6b5f60a8b3e0139e5947b4a4ad32e494e7887d4e
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- device.mk
+++ device.mk
@@ -334,11 +334,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.sm6150-libperfmgr \
-    vendor.qti.hardware.perf@2.0.vendor
+    android.hardware.power-service.sm6150-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QTI
