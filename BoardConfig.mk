4edcd1a76dc54dd9d876dbf4232e0150c0dcf923
Peter Cai <peter@typeblog.net>
2020-02-27 21:13:21 +0800

    violet: Panic to recovery, not bootloader
    Change-Id: I614dae74dfe603fc2e9256b83e2c0dc72c846686
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: raghavt20 <raghavtandon2000@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 swiotlb=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
