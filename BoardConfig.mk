97d0194b9ec562811dc5c1f0a10adaf730b3985a
Arian <arian.kulmer@web.de>
2020-03-23 21:42:04 +0100

    davinci: Configure architecture and platform
    Change-Id: I44382e290d6997fcc97f2b879c2b1c299703a572

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,3 +22,26 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Inherit from proprietary files
 include vendor/xiaomi/davinci/BoardConfigVendor.mk
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sm6150
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := sm6150
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
