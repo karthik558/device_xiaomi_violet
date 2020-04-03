ddd2142bf1a2633721bd5e20d3643cca9b84dbbe
Arian <arian.kulmer@web.de>
2020-04-04 01:07:18 +0200

    davinci: camera: Build Snap
    Change-Id: I3b1ca243276045897335b1b26142f27c0f448d06

--- device.mk
+++ device.mk
@@ -37,6 +37,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor
 
+PRODUCT_PACKAGES += \
+    Snap
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
