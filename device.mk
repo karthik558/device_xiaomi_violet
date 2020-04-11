f4e715dc98fc4e7f42791c771c25f7b7465392fd
Arian <arian.kulmer@web.de>
2020-04-11 14:08:45 +0200

    davinci: vibrator: Build HAL
    Change-Id: I3c3200a3ad5b9410ee861a981352b4d7eddf5e53

--- device.mk
+++ device.mk
@@ -328,6 +328,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
