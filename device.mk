8c75fd775c840f0e07c7ffb1d6d46d5720e8c833
Arian <arian.kulmer@web.de>
2020-04-14 16:36:51 +0200

    davinci: nfc: Import configs
    Change-Id: I965f7ec1069e0d9de161fbd9d909cf3d5ee2e6e9

--- device.mk
+++ device.mk
@@ -191,6 +191,10 @@ PRODUCT_PACKAGES += \
     nfc_nci.nqx.default.hw \
     vendor.nxp.hardware.nfc@1.2-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
