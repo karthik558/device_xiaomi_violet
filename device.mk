c5b716ac1d60180c622e1cb04183c1d591163f07
Arian <arian.kulmer@web.de>
2020-04-15 14:51:05 +0200

    davinci: graphics: Build libvulkan
    Change-Id: I6c579062982f20bc8223035d76e1e1e926122384

--- device.mk
+++ device.mk
@@ -214,3 +214,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+
+# Vulkan
+PRODUCT_PACKAGES += \
+    libvulkan
