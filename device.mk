9daef7d18e25ad07a876f1b6baeef5336faf05cf
Arian <arian.kulmer@web.de>
2020-04-16 11:52:10 +0200

    davinci: camera: Build libdng_sdk.vendor
    Change-Id: I84bd86e25f8be413dbab30da31a6858b109ea147

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    libdng_sdk.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
