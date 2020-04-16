1c8aea0c6497988a1055b3aef240bf830156aa44
Arian <arian.kulmer@web.de>
2020-04-16 11:58:05 +0200

    davinci: graphics: Build libtinyxml from source
    Change-Id: I2eb407dedf02ec277277c9681552011f87f16caf

--- device.mk
+++ device.mk
@@ -121,6 +121,9 @@ PRODUCT_PACKAGES += \
     hwcomposer.sm6150 \
     memtrack.sm6150
 
+PRODUCT_PACKAGES += \
+    libtinyxml
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
