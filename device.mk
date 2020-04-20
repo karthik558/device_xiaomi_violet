f83ffb4f07881a7aee0c2200402b4bdc83cc315a
Arian <arian.kulmer@web.de>
2020-04-21 01:09:07 +0200

    davinci: touchscreen: Copy hardware permissions from frameworks/native
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Iee8dde280391c11a69e6843f9185cd967b1799a5

--- device.mk
+++ device.mk
@@ -218,6 +218,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Touchscreen
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
