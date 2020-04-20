07850efdd7af6f819dfe407c505af7e02f30f1d5
Arian <arian.kulmer@web.de>
2020-04-21 01:08:09 +0200

    davinci: usb: Copy hardware permissions from frameworks/native
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I892323509d859b7e6642ccb7befee07961bc2cd5

--- device.mk
+++ device.mk
@@ -324,6 +324,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
