644b687a03f30e7707a172a9279ce7b6aec9f887
Arian <arian.kulmer@web.de>
2020-04-21 01:06:01 +0200

    davinci: gps: Copy hardware permissions from frameworks/native
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Ie7b5fb38125b75df174690c69c80a492e6756521

--- device.mk
+++ device.mk
@@ -145,6 +145,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
