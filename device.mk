22363a500da6c0ba49fbe0210e716189b8bbf5bd
Arian <arian.kulmer@web.de>
2020-04-21 01:06:25 +0200

    davinci: fod: Copy hardware permissions from frameworks/native
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I9e535684792d900f50e9f09d550a4762061e090b

--- device.mk
+++ device.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.davinci
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # HIDL
