bd3efd90b01d067abb7708f94b0c2455f902b3e9
Arian <arian.kulmer@web.de>
2020-04-21 21:32:39 +0200

    davinci: Import manifest and compatibility matrix
    Change-Id: I3466b07141b6517a4d83f99fe700a214bc09a93c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,10 @@ TARGET_NO_BOOTLOADER := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
