fcf43c31930aefc4f4b9ea3b2aa85724f9d03ada
Arian <arian.kulmer@web.de>
2020-04-21 16:23:51 +0200

    davinci: Import and sort vendor and odm properties
    Change-Id: I83fc839a73c24f41dbb43e3c536c079f924dcc7f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,9 @@ TARGET_BOARD_PLATFORM := sm6150
 TARGET_POWER_SET_FEATURE_LIB := libpower_feature.davinci
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
