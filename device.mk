5de047f6e970ca16e20fef5f9baf2883d6228300
Arian <arian.kulmer@web.de>
2020-04-29 14:18:41 +0200

    davinci: Build livedisplay
    Change-Id: I6ca5443d061fe4139b0bab07a5ee1ba2a77e8de9

--- device.mk
+++ device.mk
@@ -195,6 +195,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.davinci
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
