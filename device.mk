fc9d2d41a3dbe41d8d60e1e3ca565d3ea0169acf
Bharat Pawar <bpawar@codeaurora.org>
2020-07-22 20:54:03 +0530

    violet: Disable vibrator HAL interface
    Disable the existing HIDL based vibrator HAL, while
    we move to vendor vibrator HAL.
    
    Change-Id: I7529dc91c2ff994b83c7b9e7ddf4df913b9205f9

--- device.mk
+++ device.mk
@@ -417,11 +417,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
