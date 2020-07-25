6e3b2395f6d47940e6101f7cd264f96a7cee876f
Erfan Abdi <erfangplus@gmail.com>
2020-07-25 16:15:59 +0430

    davinci: Disable NFC for indian devices
    * Setup davinci sku when device is global or chinese.
    * Move the nfc manifest entry to davinci hardware sku
      variant.
    
    Change-Id: I76ac75cd80f85efa6dfa68753602d934a11000a3
    Signed-off-by: Arian <arian.kulmer@web.de>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,6 +98,9 @@ LOC_HIDL_VERSION := 3.0
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+ODM_MANIFEST_SKUS += davinci
+ODM_MANIFEST_DAVINCI_FILES := \
+    $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_davinci
