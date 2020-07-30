7f26eb6232605f96fbc9325883504021f4001352
Bharat Pawar <bpawar@codeaurora.org>
2020-07-30 20:18:30 +0530

    violet: Enable LED based vibrator
    Change-Id: I079eefaefff77fe19304acc8749773febf91a7fc
    Signed-off-by: raghavt20 <raghavtandon2000@gmail.com>
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -432,6 +432,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
