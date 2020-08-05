ArianK16a/android_device_xiaomi_davinci

davinci: parts: Check if motor is busy before doing any operation
Change-Id: I9ee7c1ef0771a7653e4ec253807c1d5f62de67d6

parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -47,6 +47,7 @@ public class PopupCameraService extends Service {
     private static String mCameraState = "-1";
     private static Handler mHandler = new Handler();
     private IMotor mMotor = null;
+    private boolean mMotorBusy = false;
     private SensorManager mSensorManager;
     private Sensor mFreeFallSensor;
     private PopupCameraPreferences mPopupCameraPreferences;
@@ -153,21 +154,45 @@ public class PopupCameraService extends Service {
     }
 
     private void updateMotor(String cameraState) {
-        if (mMotor == null) return;
-        try {
-            if (cameraState.equals(openCameraState) && mMotor.getMotorStatus() == 13) {
-                lightUp();
-                playSoundEffect(openCameraState);
-                mMotor.popupMotor(1);
-                mSensorManager.registerListener(mFreeFallListener, mFreeFallSensor, SensorManager.SENSOR_DELAY_NORMAL);
-            } else if (cameraState.equals(closeCameraState) && mMotor.getMotorStatus() == 11) {
-                lightUp();
-                playSoundEffect(closeCameraState);
-                mMotor.takebackMotor(1);
-                mSensorManager.unregisterListener(mFreeFallListener, mFreeFallSensor);
+        final Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                mMotorBusy = true;
+                mHandler.postDelayed(() -> {
+                    mMotorBusy = false;
+                }, 1200);
+                if (mMotor == null) return;
+                try {
+                    if (cameraState.equals(openCameraState) && mMotor.getMotorStatus() == 13) {
+                        lightUp();
+                        playSoundEffect(openCameraState);
+                        mMotor.popupMotor(1);
+                        mSensorManager.registerListener(mFreeFallListener, mFreeFallSensor,
+                                SensorManager.SENSOR_DELAY_NORMAL);
+                    } else if (cameraState.equals(closeCameraState) && mMotor.getMotorStatus() == 11) {
+                        lightUp();
+                        playSoundEffect(closeCameraState);
+                        mMotor.takebackMotor(1);
+                        mSensorManager.unregisterListener(mFreeFallListener, mFreeFallSensor);
+                    }
+                } catch (Exception e) {
+                    // Do nothing
+                }
             }
-        } catch (Exception e) {
-            // Do nothing
+        };
+        if (mMotorBusy) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (mMotorBusy) {
+                        mHandler.postDelayed(this, 100);
+                    } else {
+                        mHandler.post(r);
+                    }
+                }
+            }, 100);
+        } else {
+            mHandler.post(r);
         }
     }
 

karthik558/device_xiaomi_violet

Change-Id: I83e383d5f6d3d8e723259bc55945aab48e5acd6c

parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -45,6 +45,7 @@ public class PopupCameraService extends Service implements Handler.Callback {
     private static final boolean DEBUG = false;
 
     private int[] mSounds;
+    private boolean mMotorBusy = false;
     private long mClosedEvent;
     private long mOpenEvent;
 
@@ -151,24 +152,45 @@ public class PopupCameraService extends Service implements Handler.Callback {
     }
 
     private void updateMotor(String cameraState) {
-        if (mMotor == null) return;
-        try {
-            if (cameraState.equals(Constants.OPEN_CAMERA_STATE)
-                    && mMotor.getMotorStatus() == Constants.MOTOR_STATUS_TAKEBACK) {
-                lightUp();
-                playSoundEffect(Constants.OPEN_CAMERA_STATE);
-                mMotor.popupMotor(1);
-                mSensorManager.registerListener(mFreeFallListener, mFreeFallSensor,
-                        SensorManager.SENSOR_DELAY_NORMAL);
-            } else if (cameraState.equals(Constants.CLOSE_CAMERA_STATE)
-                    && mMotor.getMotorStatus() == Constants.MOTOR_STATUS_POPUP) {
-                lightUp();
-                playSoundEffect(Constants.CLOSE_CAMERA_STATE);
-                mMotor.takebackMotor(1);
-                mSensorManager.unregisterListener(mFreeFallListener, mFreeFallSensor);
+        if (mMotor == null) {
+            return;
+        }
+        final Runnable r = () -> {
+            mMotorBusy = true;
+            mHandler.postDelayed(() -> mMotorBusy = false, 1200);
+            try {
+                if (cameraState.equals(Constants.OPEN_CAMERA_STATE)
+                        && mMotor.getMotorStatus() == Constants.MOTOR_STATUS_TAKEBACK) {
+                    lightUp();
+                    playSoundEffect(Constants.OPEN_CAMERA_STATE);
+                    mMotor.popupMotor(1);
+                    mSensorManager.registerListener(mFreeFallListener, mFreeFallSensor,
+                            SensorManager.SENSOR_DELAY_NORMAL);
+                } else if (cameraState.equals(Constants.CLOSE_CAMERA_STATE)
+                        && mMotor.getMotorStatus() == Constants.MOTOR_STATUS_POPUP) {
+                    lightUp();
+                    playSoundEffect(Constants.CLOSE_CAMERA_STATE);
+                    mMotor.takebackMotor(1);
+                    mSensorManager.unregisterListener(mFreeFallListener, mFreeFallSensor);
+                }
+            } catch (RemoteException e) {
+                // Do nothing
             }
-        } catch (RemoteException e) {
-            // Do nothing
+        };
+
+        if (mMotorBusy) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (mMotorBusy) {
+                        mHandler.postDelayed(this, 100);
+                    } else {
+                        mHandler.post(r);
+                    }
+                }
+            }, 100);
+        } else {
+            mHandler.post(r);
         }
     }
 