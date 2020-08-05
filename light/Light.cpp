ArianK16a/android_device_xiaomi_davinci

davinci: light: Do not turn on breathing for unlit hardware states
Change-Id: I093fb2fb81012a473eda232d3c3407d0e0791907

light/Light.cpp
@@ -95,6 +95,11 @@ static void handleNotification(const LightState& state) {
 
     /* Disable breathing or blinking */
     set(WHITE_LED BREATH, 0);
+    set(WHITE_LED BRIGHTNESS, 0);
+
+    if (!whiteBrightness) {
+        return;
+    }
 
     switch (state.flashMode) {
         case Flash::HARDWARE:

karthik558/device_xiaomi_violet

light/Light.cpp
@@ -95,6 +95,11 @@ static void handleNotification(const LightState& state) {
 
     /* Disable breathing or blinking */
     set(NOTIFICATION_LED BREATH, 0);
+    set(NOTIFICATION_LED BRIGHTNESS, 0);
+
+    if (!notificationBrightness) {
+        return;
+    }
 
     switch (state.flashMode) {
         case Flash::HARDWARE: