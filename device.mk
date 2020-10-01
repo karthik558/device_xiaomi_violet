08c6d787f56fddf2e4ffba847f1e4b7b1edc452b
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    violet: Remove packages at build time
    * AOSP Screen recorder works fine enough.
    
    Signed-off-by: karthik558 <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -391,6 +391,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensor
 PRODUCT_PACKAGES += \
     libsensorndkbridge
