b97294d58ffe8f5ec87777dad73a87c37de44cf1
Arian <arian.kulmer@web.de>
2020-10-11 16:40:18 +0200

    davinci: Build the vendor.xiaomi.hardware.motor@1.0 interface
    Change-Id: Ieda22802167fb20f54b301f3c686f158599f09eb

--- device.mk
+++ device.mk
@@ -118,6 +118,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Camera motor
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.motor@1.0.vendor
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
