64813f7c8d0a4aef178bd6b694f1796458fd8704
TheScarastic <warabhishek@gmail.com>
2020-10-15 13:18:13 +0000

    davinci: overlay: Adjust biometric dialog for 11
    Change-Id: Id8ee895e7fe8bb643f73eaa45116ecd64d697c38

--- device.mk
+++ device.mk
@@ -285,9 +285,12 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-system
 
 # Power
 PRODUCT_PACKAGES += \
