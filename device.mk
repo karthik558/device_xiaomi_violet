f5f366fadbcb705c766a9bcfc26aaffc22c11667
derfelot <derfelot.git@gmail.com>
2020-10-22 04:52:06 +0200

    violet: Exclude CarrierConfig overlays from RRO
    They don't seem to work properly as RRO
    
    Change-Id: Ib80ed6e4bab0014682d3be0f7d0e0a4faec8fa5c
    Signed-off-by: raghavt20 <raghavtandon2000@gmail.com>
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -315,6 +315,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+
 # Perf QTI component
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0 \
