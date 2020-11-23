06eeca1f485c8c3a9cf9328f6f6b1d294a83345e
Michael Bestas <mkbestas@lineageos.org>
2020-11-23 20:47:24 +0200

    Revert "davinci: textclassifier: Build smart selection models"
    * Target no longer exists
    
    This reverts commit 65d8db50aae9bdb25b3b9554d9cacb99a2a4ad41.
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f

--- device.mk
+++ device.mk
@@ -365,10 +365,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
