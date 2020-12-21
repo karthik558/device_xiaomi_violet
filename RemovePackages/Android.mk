c52b42c2abcffe4e44cedd270ef9013d2dca17a0
karthik558 <karthik.lal558@gmail.com>
2020-12-21 17:07:25 +0000

    violet:Remove somemore blot apps
    * I am thinking if there any apps , more to remove.. ;-;
    
    Signed-off-by: karthik558 <karthik.lal558@gmail.com>
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup Drive FM2 Maps MyVerizonServices OBDM_Permissions OemDmTrigger PrebuiltGmail Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio Photos Calculator Calender
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup Drive FM2 Maps MyVerizonServices OBDM_Permissions OemDmTrigger PrebuiltGmail Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio Photos Calculator Calender NfcNci TipsPrebuilt GoogleTTS Music MicropaperPrebuilt NgaResources RecorderPrebuilt SafetyHubPrebuilt WallpapersBReel2020 SoundAmplifierPrebuilt AppDirectedSMSService ConnMO DCMO USCCDM arcore talkback DevicePolicyPrebuilt AndroidAutoStubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
