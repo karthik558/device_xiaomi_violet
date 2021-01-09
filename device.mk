d348bea83aa0e63deb86903e2ea1dcb922666d23
Mathieu Chartier <mathieuc@google.com>
2021-01-09 14:21:27 +0000

    violet: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: karthik558 <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -424,6 +424,9 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
