ecdeea2c041e43386907080a6082ed1b639031c8
Aftab Ashrafi <ashrafiaftab0@gmail.com>
2021-01-09 20:20:44 +0530

    Use pixel fp and build description to pass CTS

--- arrow_violet.mk
+++ arrow_violet.mk
@@ -22,7 +22,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210105.003 7005429 release-keys" \
     PRODUCT_NAME="violet"
+    
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
