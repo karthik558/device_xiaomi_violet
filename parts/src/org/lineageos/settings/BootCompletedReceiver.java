d03d09fafff9e9288fbab619f219d06af693bc25
Arian <arian.kulmer@web.de>
2021-01-13 00:18:51 +0100

    violet: Strip popupcamera from parts
    Change-Id: I379111b22be9062ee797979960feb3087299cffa

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.popupcamera.PopupCameraUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -34,6 +33,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
-        PopupCameraUtils.startService(context);
     }
 }
