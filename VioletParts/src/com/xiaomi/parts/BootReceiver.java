047fc88eb9d1379dda4a309235fe2a4f1e364ca0
karthik558 <karthik.lal558@gmail.com>
2021-03-27 10:43:57 -0400

    violet: VioletParts: Add Selinux Switch back
    This reverts commit c022447b5ecd89e5cea5bb0e1b698f676092a208.
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- VioletParts/src/com/xiaomi/parts/BootReceiver.java
+++ VioletParts/src/com/xiaomi/parts/BootReceiver.java
@@ -36,8 +36,66 @@ import java.util.List;
 
 public class BootReceiver extends BroadcastReceiver implements Utils {
 
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+
+    private Context settingsContext = null;
+    private static final String TAG = "SettingsOnBoot";
+    private boolean mSetupRunning = false;
+    private Context mContext;
 
     public void onReceive(Context context, Intent intent) {
+
+    mContext = context;
+    ActivityManager activityManager =
+            (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    List<ActivityManager.RunningAppProcessInfo> procInfos =
+            activityManager.getRunningAppProcesses();
+    for(int i = 0; i < procInfos.size(); i++) {
+        if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
+            mSetupRunning = true;
+        }
+    }
+
+    if(!mSetupRunning) {
+        try {
+            settingsContext = context.createPackageContext("com.android.settings", 0);
+        } catch (Exception e) {
+            Log.e(TAG, "Package not found", e);
+        }
+        SharedPreferences sharedpreferences = context.getSharedPreferences("selinux_pref",
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(PREF_SELINUX_MODE)) {
+            boolean currentIsSelinuxEnforcing = SELinux.isSELinuxEnforced();
+            boolean isSelinuxEnforcing =
+                    sharedpreferences.getBoolean(PREF_SELINUX_MODE,
+                            currentIsSelinuxEnforcing);
+            if (isSelinuxEnforcing) {
+                if (!currentIsSelinuxEnforcing) {
+                    try {
+                        SuShell.runWithSuCheck("setenforce 1");
+                        showToast(context.getString(R.string.selinux_enforcing_toast_title),
+                                context);
+                    } catch (SuShell.SuDeniedException e) {
+                        showToast(context.getString(R.string.cannot_get_su), context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                if (currentIsSelinuxEnforcing) {
+                    try {
+                        SuShell.runWithSuCheck("setenforce 0");
+                        showToast(context.getString(R.string.selinux_permissive_toast_title),
+                                context);
+                    } catch (SuShell.SuDeniedException e) {
+                        showToast(context.getString(R.string.cannot_get_su), context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
         if (Settings.Secure.getInt(context.getContentResolver(), PREF_ENABLED, 0) == 1) {
             FileUtils.setValue(KCAL_ENABLE, Settings.Secure.getInt(context.getContentResolver(),
@@ -72,4 +130,8 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
         context.startService(new Intent(context, DiracService.class));
     }
 
+    private void showToast(String toastString, Context context) {
+    Toast.makeText(context, toastString, Toast.LENGTH_SHORT)
+            .show();
+    }
 }
