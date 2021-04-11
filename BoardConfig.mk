d039f495d4a639b8847bdc49cb1849b280421658
raghavt20 <raghavtandon2000@gmail.com>
2021-04-11 07:37:12 +0000

    Revert "davinci: rootdir: Remove sleep_disabled switch during boot"
    This reverts commit 512997761231f1097305542c6f31efd5ff6accc5.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,7 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa880
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 BOARD_BOOTIMG_HEADER_VERSION := 1
