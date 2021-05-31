067264584174a18373e4db9fe6ea58d86ac12c52
karthik558 <karthik.lal558@gmail.com>
2021-05-31 14:51:21 +0530

    violet: VioletParts: Remove Dirac from parts [SQUASH]
    Revert-commit: 4fb1611 , 51aeb1f
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- VioletParts/src/com/xiaomi/parts/BootReceiver.java
+++ VioletParts/src/com/xiaomi/parts/BootReceiver.java
@@ -26,7 +26,6 @@ import android.os.SELinux;
 import android.util.Log;
 import android.widget.Toast;
 import android.text.TextUtils;
-import com.xiaomi.parts.DiracUtils;
 import com.xiaomi.parts.R;
 
 import com.xiaomi.parts.kcal.Utils;
@@ -68,8 +67,6 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
         FileUtils.setValue(DeviceSettings.USB_FASTCHARGE_PATH, Settings.Secure.getInt(context.getContentResolver(),
                 DeviceSettings.PREF_USB_FASTCHARGE, 0));
 
-       	// Dirac
-        context.startService(new Intent(context, DiracService.class));
     }
 
 }
