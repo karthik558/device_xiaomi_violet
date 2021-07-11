46aa106bc3fd563c30ad4fab860876c6ed59e0ba
K A R T H I K <karthik.lal558@gmail.com>
2021-07-11 16:08:33 +0530

    violet: Update build fp to [RQ3A.210705.001]
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -41,10 +41,10 @@ IS_PHONE := true
 TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys" \
     PRODUCT_NAME="violet"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
