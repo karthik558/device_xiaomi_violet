5bab42d11d3d5f682844155382d334775f3affaa
K A R T H I K <karthik.lal558@gmail.com>
2021-07-18 15:21:57 +0530

    violet: BoardConfig: Set SELINUX_IGNORE_NEVERALLOWS := true
    * let it ignore
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,6 +198,7 @@ TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 VENDOR_SECURITY_PATCH := 2021-05-01
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
