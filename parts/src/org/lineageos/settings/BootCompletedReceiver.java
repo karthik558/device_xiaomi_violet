6722fbb12ffa6cf83a02c8d5446f6a151272a6f8
K A R T H I K <karthik.lal558@gmail.com>
2021-09-03 17:42:20 +0530

    violet: parts: Remove "DIRAC" <from settings -> sounds>
    bug: sometimes its not even turing on
    
    This reverts commit d27b0e1 , 1a7a1b3 , 9bdebbe , 83f0776 , 4337608 , c563fce , 2e2cac5 ...
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,7 +22,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -32,9 +31,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
-        DiracUtils.initialize(context);
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
     }
 }
