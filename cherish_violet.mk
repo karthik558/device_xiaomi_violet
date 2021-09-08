9fc47137ca8737d645ceb3d1d16c954f1cdc2f53
K A R T H I K <karthik.lal558@gmail.com>
2021-09-07 20:34:51 -0700

    violet: Update build FP to [RQ3A.210905.001]
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- cherish_violet.mk
+++ cherish_violet.mk
@@ -25,13 +25,13 @@ WITH_GMS := true
 PRODUCT_GENERIC_PROPERTIES += \
     ro.cherish.maintainer=KARTHIK&NIRANJAN
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys" \
     PRODUCT_NAME="violet"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
