bcd0bb4f6640870ed5f467a956e7b1651f87451c
Shubham Das <dasshubham762@gmail.com>
2021-09-25 16:11:43 +0530

    violet: Remove build fingerprint hacks * In favour of PixelPropsUtils
    Change-Id: Ia98ca4a5e67880cf5295eeaae5228b3c73eec7f7
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- cherish_violet.mk
+++ cherish_violet.mk
@@ -28,13 +28,7 @@ WITH_GMS := true
 PRODUCT_GENERIC_PROPERTIES += \
     ro.cherish.maintainer=KARTHIK&NIRANJAN
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys" \
     PRODUCT_NAME="violet"
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
