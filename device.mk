d9af6561c8f0e3d9cc023e7cb741635aa022de62
K A R T H I K <karthik.lal558@gmail.com>
2021-10-07 16:28:54 +0530

    violet: Drop "org.ifaa.android.manager"
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -244,13 +244,6 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
-# IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
